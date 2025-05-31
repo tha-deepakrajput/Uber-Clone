8968feb1bf75b9921b9d4da5cecdba916a66222e
tha-deepakrajput <deepak23@navgurukul.org>
2025-05-31 11:35:52 +0530

    Initial commit and created a server

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const port = process.env.PORT || 4000;
+const app = require("./app");
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
